ecbc4ab5a973e391c007a304d4eba62385aac005
Turgay S <tsokmen76@gmail.com>
2023-05-18 18:13:25 +0200

    CukesRunner and FailedTestRunner classes created.

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -1,4 +1,20 @@
 package com.zerobank.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/zerobank/step_definitions",
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports",
+                //"me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt"},
+        dryRun = false,
+        tags = ""
+)
 public class CukesRunner {
 }
